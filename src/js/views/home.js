dabe879fc7cfcd713b619b507c6c22a79f524615
rcbarreto <69176511+rcbarreto@users.noreply.github.com>
2024-10-31 00:07:06 +0000

    primer salvada

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+export const Home = () => {
+
+	const [ people, setPeople] = useState ([])
+	useEffect(()=>{
+		fetch("https://www.swapi.tech/api/planets")
+		.then(res => res.json())
+		.then(data => setPeople(data.results))
+		.catch(err => console.error(err))
+	},[]) 
+	return <div className="text-center mt-5">
+		<h1>Star Wars API</h1>
+		{
+			people.map((item,index)=>{
+			return(
+				<h1 key={index}> {item.name} </h1>
+			)	
+			})
+			
+		}
+		
 	</div>
-);
+};
